441109498e9671a00135b68d97149e52938e647b
Will CR <will0386@gmail.com>
2025-04-04 07:15:18 -0300

    integração com MySQL e modificação do requirements

--- techverso/settings.py
+++ techverso/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'techverso.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'techverso',
+        'USER': 'root',
+        'PASSWORD': '1003',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
